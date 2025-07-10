d604f8f6ec532eada1c5cb3959a8f0a7ae48b5cb
Fidel <fideleliudclimax@gmail.com>
2025-07-10 16:22:45 +0300

    django's fault here

--- build.sh
+++ build.sh
@@ -16,7 +16,7 @@ mkdir -p media/responses
 python manage.py collectstatic --no-input
 
 # Apply database migrations
-python manage.py makemigrations
+python manage.py makemigrations exam
 python manage.py migrate
 python manage.py populate_exam_data
 
